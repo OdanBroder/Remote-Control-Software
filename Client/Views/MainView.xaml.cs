0262eab3c5179565e12be7e00b52daa6a733a1a4
Broder <iluvstudyforever@gmail.com>
2025-03-30 10:27:22 +0000

    Update client && server

--- Client/Views/MainView.xaml.cs
+++ Client/Views/MainView.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Client.Src.Utils;
+using Client.Src.Services;
+
+
 
 namespace Client.Views
 {
